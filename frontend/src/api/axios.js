2957a6f9aa87f7a73e4b7f46386b68a3cb05eb65
Aminata2003 <sow.aminata17@ugb.edu.sn>
2026-07-19 13:35:28 +0000

    Préparation du déploiement

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
